b2a7e4a92c7ab9d1135fbd82e662b28e7430ae21
keshavendra <35345617+keshavendra@users.noreply.github.com>
2020-01-08 00:35:48 +0530

    Initial commit

---
+++ src/test/java/com/okata/spring/SpringBootOAuthClient/SpringBootOAuthClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.okata.spring.SpringBootOAuthClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootOAuthClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
